ca93a0c1369caaede1480fa6997e0e71f0a7e44d
kemuelarised <kemuelarised@gmail.com>
2026-02-27 21:48:26 +0800

    Use Postgres on Render

--- backend/knexfile.cjs
+++ backend/knexfile.cjs
@@ -1,23 +1,10 @@
-require("dotenv").config();
-
-const db = {
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-};
-
-/** @type { import("knex").Knex.Config } */
 module.exports = {
-  client: "mysql2",
-  connection: db,
+  client: "pg",
+  connection: process.env.DATABASE_URL,
   migrations: {
     directory: "./migrations",
-    tableName: "knex_migrations",
   },
   seeds: {
     directory: "./seeds",
   },
 };
-
